06e4732132982b95c1182973805fdd7e26cadd29
atsushi421 <yff81986@nifty.com>
2023-04-28 07:50:49 +0900

    feat: add spawn_sig_handler

--- original_shell/src/shell.rs
+++ original_shell/src/shell.rs
@@ -31,6 +31,18 @@ enum ShellMsg {
     Quit(i32),     // シェルを終了。i32はシェルの終了コード
 }
 
+fn spawn_sig_handler(tx: Sender<WorkerMsg>) -> Result<(), DynError> {
+    let mut signals = Signals::new(&[SIGCHLD, SIGINT, SIGTSTP])?;
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            // シグナルを受信し、workerスレッドに送信
+            tx.send(WorkerMsg::Signal(sig)).unwrap();
+        }
+    });
+
+    Ok(())
+}
+
 #[derive(Debug)]
 pub struct Shell {
     logfile: String,
